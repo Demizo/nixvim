30612b4a3222293cfc968b050666d67d30341b77
Demizo <demizodemazo@gmail.com>
2025-02-16 10:04:45 -0600

    feat: fix telescope actions

--- config/telescope.nix
+++ config/telescope.nix
@@ -3,27 +3,30 @@
     enable = true;
 
     extensions = {
-      fzf-native = { enable = true; };
+      frecency.enable = true;
+      fzf-native = {
+        enable = true;
+      };
     };
 
     keymaps = {
 
       "<leader>/" = {
-	action = "live_grep";
-	options = {
-	  desc = "Live Grep";
-	};
+        action = "live_grep";
+        options = {
+          desc = "Live Grep";
+        };
       };
 
       "<leader>:" = {
-	action = "command_history, {}";
-	options = {
-	  desc = "Command History";
-	};
+        action = "command_history";
+        options = {
+          desc = "Command History";
+        };
       };
 
       "<leader>fb" = {
-        action = "buffers, {}";
+        action = "buffers";
         options = {
           desc = "Search Buffers";
         };
@@ -37,101 +40,101 @@
       };
 
       "<leader>fr" = {
-        action = "resume, {}";
+        action = "resume";
         options = {
           desc = "Resume Search";
         };
       };
 
       "<leader>fp" = {
-        action = "oldfiles, {}";
+        action = "oldfiles";
         options = {
           desc = "Past Files";
         };
       };
 
       "<leader>fs" = {
-	action = "grep_string, {}";
-	options = {
-	  desc = "Search for Current Word"; 
-	};
+        action = "grep_string";
+        options = {
+          desc = "Search for Current Word";
+        };
       };
-      
+
       "<C-p>" = {
-        action = "git_files, {}";
+        action = "git_files";
         options = {
           desc = "Search git files";
         };
       };
 
       "<leader>gc" = {
-        action = "git_commits, {}";
+        action = "git_commits";
         options = {
           desc = "Commits";
         };
       };
 
       "<leader>gs" = {
-        action = "git_status, {}";
+        action = "git_status";
         options = {
           desc = "Status";
         };
       };
 
       "<leader>gp" = {
-	action = "git_stash, {}";
-	options.desc = "Stash";
+        action = "git_stash";
+        options.desc = "Stash";
       };
 
       "<leader>gb" = {
-	action = "git_branches";
-	options.desc = "Branches";
+        action = "git_branches";
+        options.desc = "Branches";
       };
 
       "<leader>sb" = {
-        action = "current_buffer_fuzzy_find, {}";
+        action = "current_buffer_fuzzy_find";
         options = {
           desc = "Buffer";
         };
       };
 
       "<leader>sh" = {
-        action = "help_tags, {}";
+        action = "help_tags";
         options = {
           desc = "Help pages";
         };
       };
 
       "<leader>sk" = {
-        action = "keymaps, {}";
+        action = "keymaps";
         options = {
           desc = "Keymaps";
         };
       };
-      
+
       "<leader>sq" = {
-	action = "quickfix, {}";
-	options = {
-	  desc = "Quickfix";
-	};
+        action = "quickfix";
+        options = {
+          desc = "Quickfix";
+        };
       };
 
       "<leader>sp" = {
-	action = "quickfixhistory, {}";
-	options = {
-	  desc = "Quickfix History";
-	};
+        action = "quickfixhistory";
+        options = {
+          desc = "Quickfix History";
+        };
       };
 
       "<leader>sm" = {
-        action = "marks, {}";
+        action = "marks";
         options = {
           desc = "Jump to Mark";
         };
       };
-     
+
       "<leader>sc" = {
-        action = "colorscheme, {}";
+        action = "colorscheme";
         options = {
           desc = "Colorscheme Picker";
         };
