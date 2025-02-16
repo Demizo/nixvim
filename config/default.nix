6bfa6542e712999bdd714b47ae161ced10aaafda
Demizo <demizodemazo@gmail.com>
2025-02-16 10:09:00 -0600

    feat: add basic mini modules

--- config/default.nix
+++ config/default.nix
@@ -1,7 +1,7 @@
-{pkgs, lib, ...}:
+{ pkgs, lib, ... }:
 {
   # Import all your configuration modules here
-  imports = [ 
+  imports = [
     ./bufferline.nix
     ./nvim-tree.nix
     ./keymaps.nix
@@ -12,12 +12,12 @@
   globals.mapleader = " ";
 
   colorschemes.gruvbox.enable = true;
-  
+
   opts = {
-    number = true;         # Show line numbers
+    number = true; # Show line numbers
     relativenumber = true; # Show relative line numbers
-    spell = true; 
-    shiftwidth = 2;        # Tab width should be 2
+    spell = true;
+    shiftwidth = 2; # Tab width should be 2
   };
 
   plugins = {
@@ -25,7 +25,7 @@
     toggleterm = {
       enable = true;
       settings = {
-	open_mapping = "[[<C-t>]]";
+        open_mapping = "[[<C-t>]]";
       };
     };
     lualine.enable = true;
@@ -38,15 +38,15 @@
     fidget = {
       enable = true;
       settings.progress = {
-	suppress_on_insert = true;
- 	ignore_done_already = true;
+        suppress_on_insert = true;
+        ignore_done_already = true;
       };
     };
     conform-nvim = {
       enable = true;
       settings.formatOnSave = {
-	lspFallback = true;
-	timeoutMs = 500;
+        lspFallback = true;
+        timeoutMs = 500;
       };
       settings.notify_on_error = true;
     };
@@ -57,15 +57,32 @@
     gitsigns = {
       enable = true;
       settings = {
-	current_line_blame = true;
-	trouble = true;
-      }; 
+        current_line_blame = true;
+        trouble = true;
+      };
     };
     inc-rename = {
       enable = true;
     };
     rustaceanvim.enable = true;
     flutter-tools.enable = true;
+  };
+  plugins.mini = {
+    enable = true;
+    modules = {
+      ai = { };
+      surround = {
+        mappings = {
+          add = "gsa";
+          delete = "gsd";
+          find = "gsf";
+          find_left = "gsF";
+          highlight = "gsh";
+          replace = "gsr";
+          update_n_lines = "gsn";
+        };
+      };
+    };
   };
   plugins.lsp = {
     enable = true;
@@ -74,9 +91,9 @@
       lua_ls.enable = true;
       nixd.enable = true;
       nixd.settings = {
-	formatting = {
-	  command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
-	};
+        formatting = {
+          command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+        };
       };
       dartls.enable = true;
       pyright.enable = true;
@@ -85,40 +102,40 @@
       # rust-analyzer.installRustc = true;
     };
   };
-    plugins.lsp-format.enable = true;	 
-    plugins.fzf-lua.enable = true;
-    plugins.luasnip.enable = true;
-    plugins.cmp_luasnip.enable = true;
-    plugins.which-key.enable = true;
-    plugins.cmp-spell.enable = true;
-    plugins.cmp = {
-      enable = true;
-      autoEnableSources = true;
+  plugins.lsp-format.enable = true;
+  plugins.fzf-lua.enable = true;
+  plugins.luasnip.enable = true;
+  plugins.cmp_luasnip.enable = true;
+  plugins.which-key.enable = true;
+  plugins.cmp-spell.enable = true;
+  plugins.cmp = {
+    enable = true;
+    autoEnableSources = true;
 
-      settings = {
-        mapping = {
-          __raw = ''
-            cmp.mapping.preset.insert({
-              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-              ['<C-f>'] = cmp.mapping.scroll_docs(4),
-              ['<C-Space>'] = cmp.mapping.complete(),
-              ['<C-e>'] = cmp.mapping.abort(),
-              ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            })
-          '';
-        };
-        snippet = {
-          expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-        };
-        sources = [
-          {name = "nvim_lsp";}
-          {name = "path";}
-          {name = "buffer";}
-          {name = "luasnip";}
-          {name = "cmp-spell";}
-        ];
+    settings = {
+      mapping = {
+        __raw = ''
+          cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          })
+        '';
       };
+      snippet = {
+        expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+      };
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+        { name = "luasnip"; }
+        { name = "cmp-spell"; }
+      ];
     };
+  };
   keymaps = [
     {
       key = "<leader>c";
