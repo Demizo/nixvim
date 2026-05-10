d0dde75faaed7c5bfe53ea0b6a48dcfdf4dd3cbc
Demizo <demizodemazo@gmail.com>
2026-05-09 19:08:32 -0500

    feat: enable harpoon

--- config/harpoon.nix
+++ config/harpoon.nix
@@ -2,11 +2,30 @@
   plugins.harpoon = {
     enable = true;
     enableTelescope = true;
-    keymapsSilent = true;
-    keymaps = {
-      addFile = "<leader>ha";
-      toggleQuickMenu = "<leader>hh";
-    };
   };
-}
 
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>h";
+      action = "+harpoon";
+    }
+    {
+      mode = "n";
+      key = "<leader>ha";
+      action.__raw = "function() require'harpoon':list():add() end";
+      options = {
+        desc = "Harpoon add file";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hh";
+      action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end";
+      options = {
+        desc = "Open harpoon";
+      };
+    }
+
+  ];
+}
